f278de3e164492fa05bb956aec53da3a478f8332
lorena252 <lorenaavalos252@hotmail.com>
2023-05-17 14:43:54 -0300

    i obtained data from the new operation form

--- js/main.js
+++ js/main.js
@@ -1,36 +1,71 @@
 const $ = (selector) => document.querySelector(selector)
 
 
+//Mostrar y ocultar//
 const showElement = (selector) => $(selector).classList.remove("hidden")
 const hideElement = (selector) => $(selector).classList.add("hidden")
 
+//Id para cada operaciones//
+const randomId = () => self.crypto.randomUUID()
+
+
+//-----Mandar y traer datos al localStorage----//
+//traer operaciones y categorias//
+const bringOperationsAndCategories = () => JSON.parse(localStorage.getItem(key))
+//mandar operaciones y categorias//
+const commandOperationsAndCategories = (key, array) => localStorage.setItem(key, JSON.stringify(array))
+
+//guardar informacion de operaciones 
+const saveOperationsInformation = () =>{
+return{
+    id: randomId(),
+    description: $("#description").value,
+    amount: $("#amount").value,
+    guy: $("#guy").value ,
+    category: $("#category").value ,
+    date: $("#date").value
+
+}
+}
+
+// console.log(saveOperationsInformation())
+
+
+
+
+
+
 
 //Mostrar y ocultar secciones//
-$("#categories").addEventListener("click", () => {
+const openSaved = () =>{
+  $("#categories").addEventListener("click", () => {
     showElement(".show-categories")
     hideElement(".show-balance")
     hideElement(".show-reports")
     hideElement(".show-operation")
-})
+  })
 
-$("#balance").addEventListener("click", () => {
+  $("#balance").addEventListener("click", () => {
     showElement(".show-balance")
     hideElement(".show-categories")
     hideElement(".show-reports")
     hideElement(".show-operation")
-})
+  })
 
-$("#reports").addEventListener("click", () => {
-showElement(".show-reports")
-hideElement(".show-balance")
-hideElement(".show-categories")
-hideElement(".show-operation")
-})
-
-$("#new-operation").addEventListener("click", ()=>{
-    showElement(".show-operation")
+   $("#reports").addEventListener("click", () => {
+    showElement(".show-reports")
     hideElement(".show-balance")
     hideElement(".show-categories")
-    hideElement(".container-no-results")
+    hideElement(".show-operation")
+  })
 
-})
+   $("#new-operation").addEventListener("click", ()=>{
+   showElement(".show-operation")
+   hideElement(".show-balance")
+   hideElement(".show-categories")
+   hideElement(".container-no-results")
+  })
+
+}
+
+window.addEventListener("load", openSaved)
